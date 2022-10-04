0512ce9f9d18cd0921d6ac384adc55cd61bad90f
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-10-04 23:55:51 +0530

    0012

---
+++ Scalar/0012_Array - Prefix sum and carry forward/Special Subsequences AG.java
@@ -0,0 +1,74 @@
+/*
+ * Problem Description
+ * You have given a string A having Uppercase English letters.
+ * 
+ * You have to find how many times subsequence "AG" is there in the given
+ * string.
+ * 
+ * NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
+ * 
+ * 
+ * 
+ * Problem Constraints
+ * 1 <= length(A) <= 105
+ * 
+ * 
+ * 
+ * Input Format
+ * First and only argument is a string A.
+ * 
+ * 
+ * 
+ * Output Format
+ * Return an integer denoting the answer.
+ * 
+ * 
+ * 
+ * Example Input
+ * Input 1:
+ * 
+ * A = "ABCGAG"
+ * Input 2:
+ * 
+ * A = "GAB"
+ * 
+ * 
+ * Example Output
+ * Output 1:
+ * 
+ * 3
+ * Output 2:
+ * 
+ * 0
+ * 
+ * 
+ * Example Explanation
+ * Explanation 1:
+ * 
+ * Subsequence "AG" is 3 times in given string
+ * Explanation 2:
+ * 
+ * There is no subsequence "AG" in the given string.
+ * 
+ * 
+ */
+
+public class Solution {
+    public int solve(String A) {
+
+        int n = A.length();
+        int mod = 1000000007;
+        int count = 0;
+        int ans = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (A.charAt(i) == 'G') {
+                count++;
+            } else if (A.charAt(i) == 'A') {
+                ans += count;
+                ans %= mod;
+            }
+        }
+        return ans;
+    }
+}
