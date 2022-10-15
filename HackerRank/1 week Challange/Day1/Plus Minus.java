b03e0e7b2df162e915bfc975912244b405ec2d2c
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-10-15 08:47:47 +0530

    Update Plus Minus.java

--- HackerRank/1 week Challange/Day1/Plus Minus.java
+++ HackerRank/1 week Challange/Day1/Plus Minus.java
@@ -51,6 +51,10 @@ Explanation
 There are  positive numbers,  negative numbers, and  zero in the array.
 The proportions of occurrence are positive: , negative:  and zeros: .
   */
+
+//https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
