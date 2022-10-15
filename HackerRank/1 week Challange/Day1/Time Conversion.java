b2ae6f1c5f897196807474009ec041747993040b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-10-15 09:50:45 +0530

    Update Time Conversion.java

--- HackerRank/1 week Challange/Day1/Time Conversion.java
+++ HackerRank/1 week Challange/Day1/Time Conversion.java
@@ -38,7 +38,7 @@ Sample Output
 La
 */
 
-//https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+//https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem
 
 
 import java.io.*;
