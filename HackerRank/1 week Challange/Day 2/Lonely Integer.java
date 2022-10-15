2b1c4dc5037b9f2f782d9d9ff9ab3c74d831034b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-10-15 09:48:51 +0530

    Create Lonely Integer.java

---
+++ HackerRank/1 week Challange/Day 2/Lonely Integer.java
@@ -0,0 +1,89 @@
+/*
+Given an array of integers, where all elements but one occur twice, find the unique element.
+
+Example
+
+The unique element is .
+
+Function Description
+
+Complete the lonelyinteger function in the editor below.
+
+lonelyinteger has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the element that occurs only once
+Input Format
+
+The first line contains a single integer, , the number of integers in the array.
+The second line contains  space-separated integers that describe the values in .
+
+Constraints
+
+It is guaranteed that  is an odd number and that there is one unique element.
+, where .
+*/
+
+//https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D%5B%5D=preparation-kits&playlist_slugs%5B%5D%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D%5B%5D=one-week-day-two
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+        int xor=0;
+        
+        int n=a.size();
+        
+        for(int i=0;i<n;i++){
+            xor^=a.get(i);
+        }
+        
+        return xor;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
