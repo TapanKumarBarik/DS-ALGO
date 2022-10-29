2529a5e05417c228f530ad97c404ca5c5f99beb1
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-10-29 08:20:52 +0530

    Create Select By ID.sql

---
+++ SQL_Hackerrank/Select By ID.sql
@@ -0,0 +1,9 @@
+--Query all columns for a city in CITY with the ID 1661.
+
+--The CITY table is described as follows:
+
+--https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg
+
+select * from city
+
+where id=1661
